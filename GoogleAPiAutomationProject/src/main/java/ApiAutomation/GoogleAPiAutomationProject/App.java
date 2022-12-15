05b8300adebeb568e5a0f21b0950ad6436faa0f3
Adarsh Shrivastava <sadarsh96@gmail.com>
2022-12-15 20:10:18 +0530

    initial file architecture

---
+++ GoogleAPiAutomationProject/src/main/java/ApiAutomation/GoogleAPiAutomationProject/App.java
@@ -0,0 +1,13 @@
+package ApiAutomation.GoogleAPiAutomationProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
